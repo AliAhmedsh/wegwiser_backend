be8037fe52e399d421f2d48a4ec8d71ffa44ecec
Ali <140872322+AliAhmedsh@users.noreply.github.com>
2026-03-03 03:24:10 +0500

    updated

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3001;
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3002'],
+  origin: ['http://localhost:3000', 'http://localhost:3002', 'exp://localhost:8081'],
   credentials: true
 }));
 app.use(express.json());
